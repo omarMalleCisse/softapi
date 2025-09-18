a3beebe978e5ecfb60b7324b0537a3d7c28bf3b2
omar Malle Cisse <cissemalle58@gmail.com>
2025-09-18 20:41:21 +0000

    deploye sur 1

--- src/config.py
+++ src/config.py
@@ -1,12 +1,15 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    SECRET_KEY: str
-    ALGORITHM: str
-    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    """
+    Paramètres de configuration pour l'application, chargés à partir d'un fichier .env.
+    """
+    SECRET_KEY: str  # La clé secrète pour signer les JWT.
+    ALGORITHM: str  # L'algorithme à utiliser pour signer les JWT (par exemple, HS256).
+    ACCESS_TOKEN_EXPIRE_MINUTES: int  # La durée de vie des jetons d'accès en minutes.
 
     class Config:
-        env_file = ".env"  # utilisé seulement en local
+        env_file = ".env"
+        env_file_encoding = 'utf-8'
 
-settings = Settings()
-
+settings = Settings(_env_file='c:\\Users\\DELL\\Desktop\\api-fast\\env\\.env')
