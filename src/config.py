3ce8c066c053d7d7e6fb4dd389798acfba356d1c
omar Malle Cisse <cissemalle58@gmail.com>
2025-09-18 19:39:12 +0000

    Correction config settings

--- src/config.py
+++ src/config.py
@@ -1,15 +1,12 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    """
-    Paramètres de configuration pour l'application, chargés à partir d'un fichier .env.
-    """
-    SECRET_KEY: str  # La clé secrète pour signer les JWT.
-    ALGORITHM: str  # L'algorithme à utiliser pour signer les JWT (par exemple, HS256).
-    ACCESS_TOKEN_EXPIRE_MINUTES: int  # La durée de vie des jetons d'accès en minutes.
+    SECRET_KEY: str
+    ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
 
     class Config:
-        env_file = ".env"
-        env_file_encoding = 'utf-8'
+        env_file = ".env"  # utilisé seulement en local
 
-settings = Settings(_env_file='c:\\Users\\DELL\\Desktop\\api-fast\\env\\.env')
+settings = Settings()
+
